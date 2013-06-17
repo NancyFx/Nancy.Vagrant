57b371d2e7d5955dcf7202b959eaa082f5c1d769
Steven Robbins <ste.robbins@gmail.com>
2013-06-17 21:37:50 +0100

    Initial commit

---
+++ NancyMono2.x/Vagrantfile
@@ -0,0 +1,8 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure("2") do |config|
+  config.vm.box = "precise32"
+  config.vm.box_url = "http://files.vagrantup.com/precise32.box"
+  config.vm.provision :shell, :path => "bootstrap.sh"
+end
---
+++ NancyMono2.x/bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+apt-get update
+apt-get upgrade
+apt-get install -y mono-complete
+apt-get install -y ruby
+gem install rake
+gem install albacore
+cd /vagrant
+echo cd \/vagrant > /home/vagrant/.bashrc
+rm -rf /etc/motd
+
+echo Nancy on Mono >> /etc/motd
+echo +++++++++++++ >> /etc/motd
+echo >> /etc/motd
+echo The directory you are no in is shared with the host \(the same directory as you copied the vagrantfile into\) - on your local machine clone the source as you would normally then inside this VM type \"cd \[directory name\]\" then \"rake mono\" and observe the output. >> /etc/motd
