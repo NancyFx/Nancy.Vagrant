c2b9aabd3c6cdc9007b821f97e88272a59ed720d
Mark Higham <mark.higham@gmail.com>
2016-04-06 16:32:34 +0100

    installing ruby, rvm and mono via bootstrappers

---
+++ NancyMono4.x/install-mono.sh
@@ -0,0 +1,8 @@
+ #!/usr/bin/env bash
+
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/debian wheezy main" | tee /etc/apt/sources.list.d/mono-xamarin.list
+apt-get update
+
+apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
+apt-get install -y mono-devel
