08c6a588a1a683ebde70ebad2476965adedf2b34
Mark Higham <mark.higham@gmail.com>
2016-04-06 09:28:05 +0100

    initial v4 files

---
+++ NancyMono4.x/bootstrap.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+apt-get update
+apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
+
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 
+curl -L https://get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
+echo "source ~/.rvm/scripts/rvm" >> ~/.bashrc
+
+rvm install 2.1.2
+rvm use 2.1.2 --default
+
+gem install rake
+gem install albacore
+
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/debian wheezy main" | tee /etc/apt/sources.list.d/mono-xamarin.list
+apt-get update
+apt-get install -y mono-devel
